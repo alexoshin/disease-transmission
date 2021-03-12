4153afb4faec41dc5a47d354e0697d06e2b5c42e
Alex Oshin <alexoshin@gatech.edu>
2021-03-11 23:04:46 -0500

    Add instructions (#2)
    * Add instructions text
    
    * Update canvas resolution

--- js/sir_main.js
+++ js/sir_main.js
@@ -3,5 +3,11 @@ function onload() {
     document.body.style.opacity = 1;
     sirCanvas = setup_sir_canvas(document.getElementById('sir-canvas'), 50, 50);
     sirCanvas.start();
+    var instructions = document.getElementById('instructions');
+    instructions.innerText = '----- Key commands -----\n' +
+        '1-6: change simulation\n' +
+        'r: reset simulation\n' +
+        'p: pause/unpause\n' +
+        's: pause and step forward';
 }
 window.addEventListener('load', onload);
